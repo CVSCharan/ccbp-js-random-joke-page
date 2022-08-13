3c8476cfa670177792a0db4f538021d0bf1e3834
CVSCharan <charan.cvs@gmail.com>
2022-08-14 00:41:25 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [joke, setJoke] = useState(
+    " Click the button below to get a random Joke!"
+  );
+  const [jokesCount, setJokesCount] = useState(0);
+
+  useEffect(() => {
+    let url = "https://apis.ccbp.in/jokes/random";
+    let options = {
+      method: "GET",
+    };
+    fetch(url, options)
+      .then(function (response) {
+        return response.text();
+      })
+      .then(function (Data) {
+        console.log(JSON.parse(Data).value);
+        setJoke(JSON.parse(Data).value);
+      });
+  }, [jokesCount]);
+
+  const onClickButton = () => {
+    setJokesCount(jokesCount + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="bg-container p-3 d-flex flex-column justify-content-center text-center">
+      <img
+        src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/random-joke-img.png"
+        class="w-25 ml-auto mr-auto"
+        alt="img"
+      />
+      <p id="jokeText" class="joke-text mt-4">
+        {joke}
+      </p>
+      <div class="d-none mt-5 mb-5" id="spinner">
+        <div class="d-flex flex-row justify-content-center">
+          <div class="spinner-border" role="status"></div>
+        </div>
+      </div>
+      <button
+        id="jokeBtn"
+        class="joke-button p-1 ml-auto mr-auto mt-3"
+        onClick={onClickButton}
+      >
+        Joke
+      </button>
     </div>
   );
 }
